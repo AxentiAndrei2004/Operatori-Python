e50cedc94dad207cda410b163e10f470d5af0515
AxentiAndrei2004 <71274413+AxentiAndrei2004@users.noreply.github.com>
2020-09-21 21:20:27 +0300

    Add files via upload

---
+++ Problema 4.py
@@ -0,0 +1,4 @@
+n=int(input('Numarul natural n='))
+print ('Transformarea din metri in centimetri este', n*100)
+print('Transformarea din kilograme in miligrame este', n*1000000)
+print('Transformarea din ani in luni,saptamini,zile este',n*12,n*52,n*365)
